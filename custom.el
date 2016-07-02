13a40c71177c1b835332312899cc844168a5864b
Glauber Prado <glauber.prado83@gmail.com>
2016-07-02 10:14:53 -0300

    added new font config

--- custom.el
+++ custom.el
@@ -66,6 +66,7 @@ inversion of gas-comment-region"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-firefox))
  '(circe-default-nick "gla")
  '(circe-reduce-lurker-spam t)
  '(custom-safe-themes
@@ -97,6 +98,8 @@ inversion of gas-comment-region"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 128 :width normal :foundry "PfEd" :family "Aerial Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 168 :width normal :foundry "PfEd" :family "Fantasque Sans Mono"))))
+ '(ac-emacs-eclim-candidate-face ((t (:inherit ac-candidate-face))))
+ '(ac-emacs-eclim-selection-face ((t (:inherit ac-selection-face))))
  '(show-paren-match ((t (:background "#222222"))))
  '(sp-show-pair-match-face ((t (:background "grey33")))))
